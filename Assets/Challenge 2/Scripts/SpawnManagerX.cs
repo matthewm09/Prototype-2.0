6dac4c95d3202969a4ff60bc35e998dc4775b09c
Matthew009 <matthewm6775@nycstudents.net>
2025-10-22 12:08:11 -0400

    Completed Challenge 2
    The dog can catch the ball

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    public int ballIndex;
+
+    private float spawnLimitXLeft = -37;
+    private float spawnLimitXRight = 8;
+    private float spawnPosZ = -4;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int ballIndex = Random.Range(0,2); 
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight),26,spawnPosZ);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+    }
+
+}
